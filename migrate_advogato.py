4547b97885d61075aae94772f9477bc0c2dcb861
Dan Connolly <dconnolly@kumc.edu>
2013-01-16 21:49:23 -0600

    refactor migrate_advogato to reduce authority

--- migrate_advogato.py
+++ migrate_advogato.py
@@ -1,45 +1,56 @@
 
-from datetime import datetime
+from datetime import datetime  # not easy to tame .now(); darn
 
 
-def main():
-    import sys
-    import os
-    src, dest = sys.argv[1:3]
-    j = Journal(src,
-                os.listdir, os.path.join,
-                os.path.getmtime, open)
-    mkpages(dest, j, os.path.join, open)
+def page_saver(openw):
+    def save(name, title, date, what):
+        out = openw(name[:-len('.html')] + '.md')
+        out.write(MARKDOWN_YAML_TEMPLATE % (title, date, what))
+    return save
+
+MARKDOWN_YAML_TEMPLATE = '''
+title: %s
+date: %s
+published: true
+
+%s'''.strip()
 
 
-def mkpages(dest, j, join, open):
+def mkpages(j, save):
     for name, when, what in j:
-        where = join(dest, name[:-len('.html')] + '.md')
-        out = open(where, 'w')
         title = when.date().ctime()
         date = str(when.date())
-        out.write('title: %s\ndate: %s\npublished: true\n\n %s' % (
-            title, date, what))
+        save(name, title, date, what)
 
 
 class Journal(object):
-    def __init__(self, path, listdir, join, getmtime, open):
-        self.path = path
+    def __init__(self, listdir, what_when):
         self.__listdir = listdir
-        self.__getmtime = getmtime
-        self.__join = join
-        self.__open = open
+        self.__what_when = what_when
 
     def __iter__(self):
-        for n in self.__listdir(self.path):
+        for n in self.__listdir():
             if not n.endswith('.html'):
                 continue
-            where = self.__join(self.path, n)
-            mtsec = self.__getmtime(where)
+            what, mtsec = self.__what_when(n)
             when = datetime.fromtimestamp(mtsec)
-            what = self.__open(where).read()
             yield (n, when, what)
 
 
 if __name__ == '__main__':
-    main()
+    def _initial_caps():
+        import sys
+        import os
+        from os import path
+        src, dest = sys.argv[1:3]
+
+        def what_when(n):
+            where = path.join(src, n)
+            return open(where).read(), path.getmtime(where)
+
+        return dict(j=Journal(lambda:
+                                  os.listdir(src), what_when),
+                    save=page_saver(lambda name:
+                                        open(os.path.join(dest, name), 'w')))
+
+    mkpages(**_initial_caps())
