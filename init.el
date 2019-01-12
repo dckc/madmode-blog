b0f6ad73c9da153bf3ba4fe1819862ead831d610
Dan Connolly <dckc@madmode.com>
2019-01-12 12:10:49 -0600

    initial commit: js-dev-flow

---
+++ init.el
@@ -0,0 +1,11 @@
+;; Added by Package.el.
+(package-initialize)
+
+;; https://www.emacswiki.org/emacs/LoadPath
+(add-to-list 'load-path "~/.emacs.d/dev-dckc/")
+
+(require 'tabs-are-evil)
+
+(require 'js-dev-flow)
+
+; (load-file "scala-dev-ensime.el")
