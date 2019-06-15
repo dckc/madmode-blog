7d83a09faeefbd679f0d57bcb2df1862aef4fabd
Dan Connolly <dckc@madmode.com>
2019-06-15 10:13:18 -0500

    selected packages. hm.

--- init.el
+++ init.el
@@ -9,6 +9,14 @@
 (require 'js-dev-flow)
 
 ; (load-file "scala-dev-ensime.el")
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (yaml-mode use-package tuareg systemd pyvenv php-mode markdown-mode magit lsp-ocaml json-mode js2-mode iedit helm-idris flymake-php flycheck-pyflakes flycheck-ocaml flycheck-mypy flycheck-flow ensime csv-mode calfw-org calfw-ical calfw-gcal calfw auto-complete add-node-modules-path))))
 (put 'narrow-to-region 'disabled nil)
 
 
