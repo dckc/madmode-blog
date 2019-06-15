7fe6c16656577fafa5dd8cc331b97a283d3f1489
Dan Connolly <dckc@madmode.com>
2019-06-15 10:12:21 -0500

    Do not show ^M in files containing mixed UNIX and DOS line endings.

--- init.el
+++ init.el
@@ -9,3 +9,9 @@
 (require 'js-dev-flow)
 
 ; (load-file "scala-dev-ensime.el")
+;;; https://stackoverflow.com/a/750933/7963
+(defun remove-dos-eol ()
+  "Do not show ^M in files containing mixed UNIX and DOS line endings."
+  (interactive)
+  (setq buffer-display-table (make-display-table))
+  (aset buffer-display-table ?\^M []))
