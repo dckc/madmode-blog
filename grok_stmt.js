ff4232114673b35ed772374eeb4eca84200af812
Dan Connolly <dckc@madmode.com>
2019-07-01 18:31:37 -0500

    parse portfolio change detail cells

--- grok_stmt.js
+++ grok_stmt.js
@@ -3,21 +3,49 @@ import pathlib from './pathlib';
 import pdf from 'pdf-parse';
 
 async function main(argv, { readFile, join }) {
-    console.log('argv:', argv);
+    // console.log('argv:', argv);
     const stmt = argv[2];
     const rd = pathlib.fsReadAccess(stmt, readFile, join);
 
     const raw = await rd.readBytes();
     console.log('raw bytes: ', raw.length);
+    // https://www.npmjs.com/package/parse-pdf
+    // based on https://mozilla.github.io/pdf.js/getting_started/
     const data = await pdf(raw);
-    console.log('// number of pages', data.numpages);
-    console.log('// number of rendered pages', data.numrender);
-    
-    console.log('// PDF info', data.info);
-    console.log('// PDF metadata', data.metadata); 
-    // check https://mozilla.github.io/pdf.js/getting_started/
-    console.log('// PDF.js version', data.version);
-    console.log('// PDF text', data.text);
+
+    // console.log('// PDF text', data.text);
+    const rows = portfolioChanges(data.text);
+    console.log(rows);
+}
+
+
+function portfolioChanges(text) {
+    let state = null;
+    let rows = [];
+    let row = [];
+    for (const line of text.split('\n')) {
+        if (state === null && line.trim() === 'PORTFOLIO CHANGES') {
+            state = 'head1';
+        } else if (line.trim() === 'Since Inception') {
+            break;
+        } else if (state === 'head1') {
+            row.push(line.trim());
+            state = 'head23';
+        } else if (state === 'head23') {
+            const slash = row[0].indexOf('/');
+            row.push(line.slice(0, slash));
+            row.push(line.trim().slice(slash));
+            rows.push(row);
+            row = [];
+            state = 'body';
+        } else if (state === 'body') {
+            // console.log('line:', line);
+            const parts = line.trim().match(
+                    /([^\-0-9,$]+)([\- 0-9,$]+\.[0-9]{2})(.*)/);
+            rows.push(parts.slice(1, 4));
+        }
+    }
+    return rows;
 }
 
 
@@ -27,6 +55,6 @@ if (require.main === module) {
         readFile: require('fs').readFile,
         join: require('path').join,
     })
-        .then((it) => { console.log(it); })
+        .then(_ => null)
         .catch((oops) => { console.error(oops); });
 }
