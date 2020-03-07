80a574d56b2e5ff1bd22eee8bc2c0aa013f0075b
Dan Connolly <dckc@madmode.com>
2020-03-07 10:32:04 -0600

    moddable include path; js-indent offset (ew)
    should be local to agoric / moddable projects

--- init.el
+++ init.el
@@ -29,6 +29,17 @@
  '(package-selected-packages
    (quote
     (go-autocomplete go-mode godoctor merlin-eldoc reason-mode utop caml merlin flycheck-rust cargo rust-mode nix-mode direnv yaml-mode use-package tuareg systemd pyvenv php-mode markdown-mode magit lsp-ocaml json-mode js2-mode iedit helm-idris flymake-php flycheck-pyflakes flycheck-ocaml flycheck-mypy flycheck-flow ensime csv-mode calfw-org calfw-ical calfw-gcal calfw auto-complete add-node-modules-path)))
+ '(safe-local-variable-values
+   (quote
+    ((eval setq flycheck-clang-include-path
+           (list
+            (expand-file-name "~/projects/moddable/xs/includes/")
+            (expand-file-name "~/projects/moddable/xs/sources/")))
+     (eval setq flycheck-clang-include-path
+           (list
+            (expand-file-name "~/projects/moddable/xs/includes/")))
+     (js2-basic-offset . 2)
+     (js2-indent-switch-body . true)))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
