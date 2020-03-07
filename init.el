6a770b628facc8b3bfc08da05ce9bb4009e5b0ef
Dan Connolly <dckc@madmode.com>
2020-03-07 10:31:10 -0600

    flake8 in /usr/bin (why?)

--- init.el
+++ init.el
@@ -24,6 +24,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(flycheck-python-flake8-executable "/usr/bin/flake8")
  '(indent-tabs-mode nil)
  '(package-selected-packages
    (quote
