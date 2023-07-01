1d6330f98c40b425ee7fae2325cec6a9fb6be67d
Dan Connolly <dckc@madmode.com>
2023-07-01 12:08:06 -0500

    refactor(emacs): move remove-dos-eol to its own file

---
+++ dos-newlines.el
@@ -0,0 +1,6 @@
+;;; https://stackoverflow.com/a/750933/7963
+(defun remove-dos-eol ()
+  "Do not show ^M in files containing mixed UNIX and DOS line endings."
+  (interactive)
+  (setq buffer-display-table (make-display-table))
+  (aset buffer-display-table ?\^M []))
--- init.el
+++ init.el
@@ -47,10 +47,3 @@
  ;; If there is more than one, they won't work right.
  )
 (put 'narrow-to-region 'disabled nil)
-
-;;; https://stackoverflow.com/a/750933/7963
-(defun remove-dos-eol ()
-  "Do not show ^M in files containing mixed UNIX and DOS line endings."
-  (interactive)
-  (setq buffer-display-table (make-display-table))
-  (aset buffer-display-table ?\^M []))
