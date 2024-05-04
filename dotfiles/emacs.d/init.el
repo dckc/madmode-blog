5850b004bf5c22cdbf3f6a36a57b97e697ad4010
Dan Connolly <dckc@madmode.com>
2024-05-04 16:13:09 -0500

    chore: some ocaml emacs stuff

--- dotfiles/emacs.d/init.el
+++ dotfiles/emacs.d/init.el
@@ -24,11 +24,9 @@
  '(flycheck-python-flake8-executable "/usr/bin/flake8")
  '(indent-tabs-mode nil)
  '(package-selected-packages
-   (quote
-    (go-autocomplete go-mode godoctor merlin-eldoc reason-mode utop caml merlin flycheck-rust cargo rust-mode nix-mode direnv yaml-mode use-package tuareg systemd pyvenv php-mode markdown-mode magit lsp-ocaml json-mode js2-mode iedit helm-idris flymake-php flycheck-pyflakes flycheck-ocaml flycheck-mypy flycheck-flow ensime csv-mode calfw-org calfw-ical calfw-gcal calfw auto-complete add-node-modules-path)))
+   '(opam-switch-mode kubernetes go-autocomplete go-mode godoctor merlin-eldoc reason-mode utop caml merlin flycheck-rust cargo rust-mode nix-mode direnv yaml-mode use-package tuareg systemd pyvenv php-mode markdown-mode magit lsp-ocaml json-mode js2-mode iedit helm-idris flymake-php flycheck-pyflakes flycheck-ocaml flycheck-mypy flycheck-flow ensime csv-mode calfw-org calfw-ical calfw-gcal calfw auto-complete add-node-modules-path))
  '(safe-local-variable-values
-   (quote
-    ((eval setq flycheck-clang-include-path
+   '((eval setq flycheck-clang-include-path
            (list
             (expand-file-name "~/projects/moddable/xs/includes/")
             (expand-file-name "~/projects/moddable/xs/sources/")))
@@ -36,7 +34,7 @@
            (list
             (expand-file-name "~/projects/moddable/xs/includes/")))
      (js2-basic-offset . 2)
-     (js2-indent-switch-body . true)))))
+     (js2-indent-switch-body . true))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
