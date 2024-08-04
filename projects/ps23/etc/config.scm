ae06a99c9404ad6e90197a319c40df5dc46a8605
Dan Connolly <dckc@madmode.com>
2024-08-03 23:05:02 -0500

    style(ps23): guix style -f config.scm

--- projects/ps23/etc/config.scm
+++ projects/ps23/etc/config.scm
@@ -6,56 +6,52 @@
 (use-package-modules screen ssh)
 
 (operating-system
- (host-name "ps23")
- (timezone "America/Chicago")
- (locale "en_US.utf8")
+  (host-name "ps23")
+  (timezone "America/Chicago")
+  (locale "en_US.utf8")
 
- ;; Boot in UEFI mode.
- (bootloader (bootloader-configuration
-  (bootloader grub-efi-bootloader)
-  (targets '("/boot/efi"))
-  ))
+  ;; Boot in UEFI mode.
+  (bootloader (bootloader-configuration
+                (bootloader grub-efi-bootloader)
+                (targets '("/boot/efi"))))
 
- ;; It's fitting to support the equally bare bones ‘-nographic’
- ;; QEMU option, which also nicely sidesteps forcing QWERTY.
- (kernel-arguments (list "console=ttyS0,115200"))
- (file-systems (append
-                (list (file-system
-                        (device (file-system-label "ps23-root"))
-                        (mount-point "/")
-                        (type "ext4"))
-                      (file-system
-                        (device (file-system-label "ESP"))
-                        (mount-point "/boot/efi")
-                        (type "fat32")))
-                %base-file-systems))
+  ;; It's fitting to support the equally bare bones ‘-nographic’
+  ;; QEMU option, which also nicely sidesteps forcing QWERTY.
+  (kernel-arguments (list "console=ttyS0,115200"))
+  (file-systems (append (list (file-system
+                                (device (file-system-label "ps23-root"))
+                                (mount-point "/")
+                                (type "ext4"))
+                              (file-system
+                                (device (file-system-label "ESP"))
+                                (mount-point "/boot/efi")
+                                (type "fat32"))) %base-file-systems))
 
- ;; This is where user accounts are specified.  The "root"
- ;; account is implicit, and is initially created with the
- ;; empty password.
- (users (cons (user-account
-               (name "dckc")
-               (comment "madmode.com")
-               (group "users")
+  ;; This is where user accounts are specified.  The "root"
+  ;; account is implicit, and is initially created with the
+  ;; empty password.
+  (users (cons (user-account
+                 (name "dckc")
+                 (comment "madmode.com")
+                 (group "users")
 
-               ;; Adding the account to the "wheel" group
-               ;; makes it a sudoer.  Adding it to "audio"
-               ;; and "video" allows the user to play sound
-               ;; and access the webcam.
-               (supplementary-groups '("wheel"
-                                       "audio" "video")))
-              %base-user-accounts))
+                 ;; Adding the account to the "wheel" group
+                 ;; makes it a sudoer.  Adding it to "audio"
+                 ;; and "video" allows the user to play sound
+                 ;; and access the webcam.
+                 (supplementary-groups '("wheel" "audio" "video")))
+               %base-user-accounts))
 
- ;; Globally-installed packages.
- (packages (cons screen %base-packages))
+  ;; Globally-installed packages.
+  (packages (cons screen %base-packages))
 
- ;; Add services to the baseline: a DHCP client and
- ;; an SSH server.
- (services (append (list (service dhcp-client-service-type)
-                         (service openssh-service-type
-                                  (openssh-configuration
-                                   (openssh openssh-sans-x)
-                                   (authorized-keys
-                                    `(("dckc" ,(local-file "../dckc.keys"))))
-                                   (port-number 2222))))
-                   %base-services)))
+  ;; Add services to the baseline: a DHCP client and
+  ;; an SSH server.
+  (services
+   (append (list (service dhcp-client-service-type)
+                 (service openssh-service-type
+                          (openssh-configuration (openssh openssh-sans-x)
+                                                 (authorized-keys `(("dckc" ,(local-file
+                                                                              "../dckc.keys"))))
+                                                 (port-number 2222))))
+           %base-services)))
