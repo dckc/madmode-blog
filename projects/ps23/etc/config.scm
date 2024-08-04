6df30a617ec74873e04f6cb1116876d470976824
Dan Connolly <dckc@madmode.com>
2024-08-03 23:05:35 -0500

    refactor(ps23): re-order to match gui config

--- projects/ps23/etc/config.scm
+++ projects/ps23/etc/config.scm
@@ -6,41 +6,24 @@
 (use-package-modules screen ssh)
 
 (operating-system
-  (host-name "ps23")
-  (timezone "America/Chicago")
   (locale "en_US.utf8")
-
-  ;; Boot in UEFI mode.
-  (bootloader (bootloader-configuration
-                (bootloader grub-efi-bootloader)
-                (targets '("/boot/efi"))))
-
-  ;; It's fitting to support the equally bare bones ‘-nographic’
-  ;; QEMU option, which also nicely sidesteps forcing QWERTY.
-  (kernel-arguments (list "console=ttyS0,115200"))
-  (file-systems (append (list (file-system
-                                (device (file-system-label "ps23-root"))
-                                (mount-point "/")
-                                (type "ext4"))
-                              (file-system
-                                (device (file-system-label "ESP"))
-                                (mount-point "/boot/efi")
-                                (type "fat32"))) %base-file-systems))
+  (timezone "America/Chicago")
+  (host-name "ps23")
 
   ;; This is where user accounts are specified.  The "root"
   ;; account is implicit, and is initially created with the
   ;; empty password.
-  (users (cons (user-account
-                 (name "dckc")
-                 (comment "madmode.com")
-                 (group "users")
+  (users (cons* (user-account
+                  (name "dckc")
+                  (comment "madmode.com")
+                  (group "users")
 
-                 ;; Adding the account to the "wheel" group
-                 ;; makes it a sudoer.  Adding it to "audio"
-                 ;; and "video" allows the user to play sound
-                 ;; and access the webcam.
-                 (supplementary-groups '("wheel" "audio" "video")))
-               %base-user-accounts))
+                  ;; Adding the account to the "wheel" group
+                  ;; makes it a sudoer.  Adding it to "audio"
+                  ;; and "video" allows the user to play sound
+                  ;; and access the webcam.
+                  (supplementary-groups '("wheel" "netdev" "audio" "video")))
+                %base-user-accounts))
 
   ;; Globally-installed packages.
   (packages (cons screen %base-packages))
@@ -54,4 +37,21 @@
                                                  (authorized-keys `(("dckc" ,(local-file
                                                                               "../dckc.keys"))))
                                                  (port-number 2222))))
-           %base-services)))
+           %base-services))
+
+  ;; Boot in UEFI mode.
+  (bootloader (bootloader-configuration
+                (bootloader grub-efi-bootloader)
+                (targets (list "/boot/efi"))))
+
+  ;; It's fitting to support the equally bare bones ‘-nographic’
+  ;; QEMU option, which also nicely sidesteps forcing QWERTY.
+  (kernel-arguments (list "console=ttyS0,115200"))
+  (file-systems (cons* (file-system
+                         (mount-point "/boot/efi")
+                         (device (file-system-label "ESP"))
+                         (type "vfat"))
+                       (file-system
+                         (mount-point "/")
+                         (device (file-system-label "ps23-root"))
+                         (type "ext4")) %base-file-systems)))
