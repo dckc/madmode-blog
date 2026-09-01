6af484a2293678e5690bd7b74263ca2f6f9a0351
Dan Connolly <dckc@madmode.com>
2026-08-31 23:31:12 -0500

    build: scaffold polzap android project with nix flake dev shell

---
+++ projects/polzap/build.gradle.kts
@@ -0,0 +1,6 @@
+// SPDX-License-Identifier: CC0-1.0
+plugins {
+    alias(libs.plugins.android.application) apply false
+    alias(libs.plugins.kotlin.android) apply false
+    alias(libs.plugins.kotlin.kapt) apply false
+}
---
+++ projects/polzap/settings.gradle.kts
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: CC0-1.0
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "polzap"
+include(":app")
